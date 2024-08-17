c087f4b04e49ea9dbb4cdaa7e1f0175fc991bd80
Mother1teresa <129117416+Mother1teresa@users.noreply.github.com>
2024-08-18 00:38:29 +0300

    Модальное окно корзины, добавила стили

--- src/components/items/ModalBasket.vue
+++ src/components/items/ModalBasket.vue
@@ -3,12 +3,12 @@
     <li class="nav-item">
       <a class="nav-link" href="#">
         <span><img src="/src/assets/image/favourites.png" alt="" /></span>
-    </a>
+      </a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#">
         <span><img src="/src/assets/image/profile.png" alt="" /></span>
-    </a>
+      </a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#" @click="openModal">
@@ -22,46 +22,48 @@
         <div class="modal-title">Ваша корзина</div>
         <button class="close-button" @click="closeModal">&times;</button>
 
-        <!-- Список товаров -->
-        <div class="item" v-for="product in products" :key="product.id" >
-          <img :src="product.imageUrl" :alt="Image"/>
-          <div class="item-info">
-            <div class="item-content">
-              <div class="item-titles">
-                <div class="item-title_lamp">
+        
+        <div class="items">
+          <div class="item" v-for="product in products" :key="product.id">
+            <img :src="product.imageUrl" alt="Image" />
+            <div class="item-info">
+              <div class="item-content">
+                <div class="item-titles">
+                  <div class="item-title_lamp">
                     <div class="color"></div>
                     <div class="item-title">{{ product.title }}</div>
+                  </div>
+                  <div class="item-content_text">{{ product.text }}</div>
                 </div>
-                <div class="item-content_text">{{ product.text }}</div>
+                <a class="" href="#">
+                  <span>
+                    <img src="/src/assets/image/basket-lime.png" alt="Basket" />
+                  </span>
+                </a>
               </div>
-              <a class="nav-link" href="#">
-                <span><img src="/src/assets/image/basket-lime.png" alt="" /></span>
-              </a>
-            </div>
-            <div class="border"></div>
-            <div class="item-quantity">
-                <div class="amount-result">
-                    150 000 <span class="currency">₽</span>
+              <div class="border-line"></div>
+              <div class="item-quantity">
+                <div class="item-quantity-result">
+                  {{ product.result }}<span class="currency">₽</span>
                 </div>
                 <div class="item-button">
-                    <div class="minus-btn"></div>
-                    <span class="figure"></span>
-                    <div class="plus-btn"></div>
+                  <div class="minus-btn">-</div>
+                  <span class="figure">1</span>
+                  <div class="plus-btn">+</div>
                 </div>
+              </div>
             </div>
           </div>
-          <!-- <p>Product {{ index + 1 }}</p> -->
-          <!-- Здесь можно поместить информацию о товаре -->
-        </div>
 
-        <div class="model-result">
-          <div class="amount">
-            <div class="amount-text">Итог:</div>
-            <div class="amount-result">
-              450 000 <span class="currency">₽</span>
+          <div class="model-result">
+            <div class="amount">
+              <div class="amount-text">Итого:</div>
+              <div class="amount-result">
+                450 000 <span class="currency">₽</span>
+              </div>
             </div>
+            <button class="btn"><span class="btn-text">Оформить</span></button>
           </div>
-          <button class="btn"><span class="btn-text">Оформить</span></button>
         </div>
       </div>
     </div>
@@ -69,27 +71,39 @@
 </template>
 
 <script>
+import image1 from "/src/assets/image/sophi.png";
+import image2 from "/src/assets/image/sophistica.png";
+import image3 from "/src/assets/image/sophisticated-.png";
 export default {
   data() {
     return {
       isModalOpen: false,
       products: [
         {
-            id: 1,
-            imageUrl:"src/assets/s7.png",
-            Image:'fd',
-            color:'',
-            title:'coppelia',
-            text:'Лампа настольная',
+          id: 1,
+          imageUrl: image1,
+          color: "",
+          title: "coppelia",
+          text: "Лампа настольная",
+          result: "150 000",
         },
         {
-            id: 2,
-            imageUrl:"/src/assets/image/redLamp.jpg",
-            color:'',
-            title:'coppelia',
-            text:'Лампа настольная',
+          id: 2,
+          imageUrl: image2,
+          color: "",
+          title: "Facchetta",
+          text: "Лампа настольная",
+          result: "150 000",
         },
-    ], // Здесь можно разместить реальные данные о товарах
+        {
+          id: 3,
+          imageUrl: image3,
+          color: "",
+          title: "coppelia",
+          text: "Лампа настольная",
+          result: "150 000",
+        },
+      ], 
     };
   },
   methods: {
@@ -114,7 +128,7 @@ export default {
 .model-result {
   display: flex;
   justify-content: space-between;
-  margin-top: 83px;
+  margin-top: 23px;
 }
 .amount-text {
   font-size: 20px;
@@ -123,9 +137,42 @@ export default {
   text-transform: uppercase;
   color: rgba(255, 255, 255, 1);
 }
-.item-image img{
-    width: 89px;
-    height: 142px;
+.minus-btn,
+.figure,
+.plus-btn {
+  align-content: center;
+  color: white;
+}
+.minus-btn {
+  text-align: center;
+  width: 45px;
+  height: 45px;
+  background: rgba(255, 255, 255, 0.19);
+  border: 2px solid rgba(255, 255, 255, 1);
+  border-radius: 50%;
+  opacity: 50%;
+}
+.plus-btn {
+  text-align: center;
+  width: 45px;
+  height: 45px;
+  background: rgba(255, 255, 255, 0.19);
+  border: 2px solid rgba(255, 255, 255, 1);
+  border-radius: 50%;
+}
+.item-image img {
+  width: 89px;
+  height: 142px;
+}
+.item-info {
+  width: 393px;
+}
+.item-quantity-result {
+  font-family: "Floreste Wavy", sans-serif;
+  font-size: 28px;
+  color: rgba(255, 255, 255, 1);
+  letter-spacing: 3px;
+  margin-top: 20px;
 }
 .amount-result {
   font-family: "Floreste Wavy", sans-serif;
@@ -136,6 +183,30 @@ export default {
 }
 .currency {
   font-size: 20px;
+}
+.border-line {
+  border: 2px solid rgba(217, 255, 90, 1);
+}
+.item-button {
+  display: flex;
+  gap: 10px;
+  width: 117px;
+  height: 45px;
+  margin-top: 15px;
+}
+.item-content_text {
+  font-family: "Euclid Circular A", sans-serif;
+  font-size: 18px;
+  color: rgba(255, 255, 255, 1);
+  margin-top: 4px;
+  margin-bottom: 16px;
+}
+.item-title {
+  font-size: 20px;
+  font-family: "Euclid Circular A", sans-serif;
+  color: rgba(255, 255, 255, 1);
+  text-transform: uppercase;
+  letter-spacing: 1px;
 }
 .btn {
   width: 200px;
@@ -176,7 +247,14 @@ export default {
   transform: scale(1) rotate(8deg) translateX(0px) translateY(0px) skewX(0deg)
     skewY(0deg);
 }
-
+.item-content {
+  display: flex;
+  justify-content: space-between;
+}
+.item-quantity {
+  display: flex;
+  justify-content: space-between;
+}
 .modal-title {
   color: rgba(255, 255, 255, 1);
   font-family: "Euclid Circular A", sans-serif;
@@ -223,14 +301,17 @@ export default {
 .close-button:hover {
   opacity: 1;
 }
-/* .item {
+.item {
   width: 523px;
   height: 145px;
   padding: 10px;
   display: flex;
-
-  
-} */
+  gap: 41px;
+}
+.items {
+  display: grid;
+  gap: 60px;
+}
 .slide-enter-active,
 .slide-leave-active {
   transition: transform 1s ease;
