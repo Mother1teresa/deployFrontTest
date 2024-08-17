8284bc2e13ca873cdaeaa40bf735417ec969761e
Mother1teresa <129117416+Mother1teresa@users.noreply.github.com>
2024-08-17 14:23:57 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <AppHeader />
+  </header>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+components:{
+  AppHeader,
+}
+}
+</script>
+
+<style>
+body{
+  background: lavender;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Приве, мир</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
